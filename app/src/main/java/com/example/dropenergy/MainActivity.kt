c3a73cc876fd0bd041123adf584b3f20b3db4697
Leturgone <finekon221@gmail.com>
2024-10-25 21:36:44 +0300

    Создан макет для главного экрана

--- app/src/main/java/com/example/dropenergy/MainActivity.kt
+++ app/src/main/java/com/example/dropenergy/MainActivity.kt
@@ -1,46 +1,96 @@
 package com.example.dropenergy
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.WindowInsets.Side
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.dropenergy.ui.theme.DropEnergyTheme
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             DropEnergyTheme {
-                // A surface container using the 'background' color from the theme
+                SetBarColor(color = MaterialTheme.colorScheme.background)
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainScreen()
                 }
             }
         }
     }
 }
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen(){
+    Scaffold(
+        bottomBar = {
+            BottomNavigationBar()
+        }
+    ) {padding ->
+        Column(
+            modifier  = Modifier.fillMaxSize().padding(padding)
+        ) {
+            //CalendarSection()
+            Spacer(modifier =Modifier.height(16.dp))
+
+            //ProgressSection()
+        }
+
+    }
+
 }
+
+
+@Composable
+private fun SetBarColor(color : Color){
+    //Функция для изменения цвета статусбара
+    val systemUiController = rememberSystemUiController()
+    SideEffect {
+        systemUiController.setSystemBarsColor(color = color)
+    }
+}
+
+@Composable
+fun InitAskNameScreen(){}
+
+@Composable
+fun InitAskGenderScreen(){}
+
+
+@Composable
+fun InitAskCanScreen(){}
+
+@Composable
+fun LoginScreen(){}
+
+
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     DropEnergyTheme {
-        Greeting("Android")
     }
 }
