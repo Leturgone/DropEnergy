0d3b46e282b7f22a0a46a19e82adde85f7ee7278
Leturgone <finekon221@gmail.com>
2024-10-25 21:53:49 +0300

    Сделан список элементов для боттом бара

--- app/src/main/java/com/example/dropenergy/BottomNavigationBar.kt
+++ app/src/main/java/com/example/dropenergy/BottomNavigationBar.kt
@@ -1,5 +1,21 @@
 package com.example.dropenergy
 
-val items = listOf(
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.AddCircle
+import androidx.compose.material.icons.rounded.BarChart
+import com.example.dropenergy.data.BottomNavigation
 
+val items = listOf(
+    BottomNavigation(
+        title = "Статистика",
+        icon = Icons.Rounded.BarChart
+    ),
+    BottomNavigation(
+        title = null,
+        icon = Icons.Rounded.AddCircle
+    ),
+    BottomNavigation(
+        title = "Статистика",
+        icon = Icons.Rounded.BarChart
+    )
 )
