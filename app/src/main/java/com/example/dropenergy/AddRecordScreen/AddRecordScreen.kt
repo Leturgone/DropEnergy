6446bca741be7b0642b18eaa32aa11c92c7079d4
Leturgone <finekon221@gmail.com>
2024-10-29 02:18:25 +0300

    Сделан дизайн AddRecordScreen

--- app/src/main/java/com/example/dropenergy/AddRecordScreen/AddRecordScreen.kt
+++ app/src/main/java/com/example/dropenergy/AddRecordScreen/AddRecordScreen.kt
@@ -1,6 +1,86 @@
 package com.example.dropenergy.AddRecordScreen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
+@Preview
 @Composable
-fun AddRecordScreen(){}
+fun AddRecordScreen(){
+    Column(modifier = Modifier
+        .fillMaxWidth()){
+        Box(modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)){
+            Column(
+                modifier = Modifier
+                    .clip(RoundedCornerShape(25.dp))
+                    .background(Color.Yellow)
+                    .clickable {}
+                    .padding(10.dp).fillMaxWidth(), verticalArrangement = Arrangement.Center
+            ) {
+                Text(text = "Хочу энергетик",
+                    fontSize = 20.sp,
+                    color = MaterialTheme.colorScheme.onBackground,
+                    fontWeight = FontWeight.Medium,
+                    modifier = Modifier.padding(16.dp).fillMaxWidth(),
+                    textAlign = TextAlign.Center)
+
+            }
+        }
+        Box(modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)){
+            Column(
+                modifier = Modifier
+                    .clip(RoundedCornerShape(25.dp))
+                    .background(Color.Red)
+                    .clickable {}
+                    .padding(10.dp).fillMaxWidth(), verticalArrangement = Arrangement.Center
+            ) {
+                Text(text = "Я купил энергетик",
+                    fontSize = 20.sp,
+                    color = MaterialTheme.colorScheme.onBackground,
+                    fontWeight = FontWeight.Medium,
+                    modifier = Modifier.padding(16.dp).fillMaxWidth(),
+                    textAlign = TextAlign.Center)
+
+            }
+        }
+        Box(modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)){
+            Column(
+                modifier = Modifier
+                    .clip(RoundedCornerShape(25.dp))
+                    .background(Color.Green)
+                    .clickable {}
+                    .padding(10.dp).fillMaxWidth(), verticalArrangement = Arrangement.Center
+            ) {
+                Text(text = "Я справился с соблазном",
+                    fontSize = 20.sp,
+                    color = MaterialTheme.colorScheme.onBackground,
+                    fontWeight = FontWeight.Medium,
+                    modifier = Modifier.padding(16.dp).fillMaxWidth(),
+                    textAlign = TextAlign.Center)
+
+            }
+        }
+    }
+}
