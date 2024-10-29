25337f78673db8b06445dedba4d966f8cc35049e
Leturgone <finekon221@gmail.com>
2024-10-29 09:58:01 +0300

    Создан список записей

--- app/src/main/java/com/example/dropenergy/DiaryScreen/DiaryScreen.kt
+++ app/src/main/java/com/example/dropenergy/DiaryScreen/DiaryScreen.kt
@@ -1,6 +1,55 @@
 package com.example.dropenergy.DiaryScreen
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.dropenergy.data.DiaryRecord
 
+
+var diaryList = listOf(
+    DiaryRecord(
+        date = "29.10.24 9:55",
+        recordText = "Я Справился с соблазном",
+        intensive = 8
+    ),
+    DiaryRecord(
+        date = "28.10.24 9:55",
+        recordText = "Я Справился с соблазном",
+        intensive = 8
+    ),
+    DiaryRecord(
+        date = "26.10.24 9:55",
+        recordText = "Я купил энергетик",
+        intensive = 10
+    )
+)
+@Preview
 @Composable
-fun DiaryScreen(){}
+fun DiaryScreen(){
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
