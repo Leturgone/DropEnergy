906526d7e510b6d8fd3177e46376d3b143083b5d
Leturgone <finekon221@gmail.com>
2024-11-02 21:28:04 +0300

    Добавлены текстовые поля в экран уточнения

--- app/src/main/java/com/example/dropenergy/AddRecordScreen/NewRecordScreen.kt
+++ app/src/main/java/com/example/dropenergy/AddRecordScreen/NewRecordScreen.kt
@@ -1,8 +1,50 @@
 package com.example.dropenergy.AddRecordScreen
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Preview
 @Composable
-fun NewRecordScreen(){}
+fun NewRecordScreen(){
+    Box(
+        modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center
+    ){
+        Column {
+            Column {
+                Text(text = "Дата записи",
+                    fontSize = 24.sp,
+                    color = MaterialTheme.colorScheme.onBackground,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(16.dp)
+
+                )
+                Text(text = "Оцените интенсивность",
+                    fontSize = 24.sp,
+                    color = MaterialTheme.colorScheme.onBackground,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(16.dp)
+
+                )
+                Text(text = "Как вы себя чувствуете?",
+                    fontSize = 24.sp,
+                    color = MaterialTheme.colorScheme.onBackground,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(16.dp)
+
+                )
+
+            }
+        }
+    }
+}
