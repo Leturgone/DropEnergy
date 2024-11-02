01ee50d65043972d218a27ce615eba9020871f32
Leturgone <finekon221@gmail.com>
2024-11-02 21:10:18 +0300

    Создан файл для экрана с уточнением создаваемой записи

---
+++ app/src/main/java/com/example/dropenergy/AddRecordScreen/NewRecordScreen.kt
@@ -0,0 +1,8 @@
+package com.example.dropenergy.AddRecordScreen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun NewRecordScreen(){}
