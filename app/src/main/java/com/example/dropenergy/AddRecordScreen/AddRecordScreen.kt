be8a89f3d5cbb1f641c617d93c007d53e16a2f3c
Leturgone <finekon221@gmail.com>
2024-11-02 21:01:59 +0300

    Сделано центрирования для AddRecordScreen

--- app/src/main/java/com/example/dropenergy/AddRecordScreen/AddRecordScreen.kt
+++ app/src/main/java/com/example/dropenergy/AddRecordScreen/AddRecordScreen.kt
@@ -5,12 +5,14 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -23,61 +25,71 @@ import androidx.compose.ui.unit.sp
 @Preview
 @Composable
 fun AddRecordScreen(){
-    Column(modifier = Modifier
-        .fillMaxWidth()){
-        Box(modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp)
-            .clickable {  }){
-            Column(
-                modifier = Modifier
-                    .clip(RoundedCornerShape(25.dp))
-                    .background(Color.Yellow)
-                    .padding(10.dp).fillMaxWidth(), verticalArrangement = Arrangement.Center
-            ) {
-                Text(text = "Я хочу энергетик",
-                    fontSize = 20.sp,
-                    color = MaterialTheme.colorScheme.onBackground,
-                    fontWeight = FontWeight.Medium,
-                    modifier = Modifier.padding(16.dp).fillMaxWidth(),
-                    textAlign = TextAlign.Center)
+    Box(
+        modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Box(modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .clickable { }) {
+                Column(
+                    modifier = Modifier
+                        .clip(RoundedCornerShape(25.dp))
+                        .background(Color.Yellow)
+                        .padding(10.dp).fillMaxWidth(), verticalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        text = "Я хочу энергетик",
+                        fontSize = 20.sp,
+                        color = MaterialTheme.colorScheme.onBackground,
+                        fontWeight = FontWeight.Medium,
+                        modifier = Modifier.padding(16.dp).fillMaxWidth(),
+                        textAlign = TextAlign.Center
+                    )
 
+                }
             }
-        }
-        Box(modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp).clickable {  }){
-            Column(
-                modifier = Modifier
-                    .clip(RoundedCornerShape(25.dp))
-                    .background(Color.Red)
-                    .padding(10.dp).fillMaxWidth(), verticalArrangement = Arrangement.Center
-            ) {
-                Text(text = "Я купил энергетик",
-                    fontSize = 20.sp,
-                    color = MaterialTheme.colorScheme.onBackground,
-                    fontWeight = FontWeight.Medium,
-                    modifier = Modifier.padding(16.dp).fillMaxWidth(),
-                    textAlign = TextAlign.Center)
+            Box(modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp).clickable { }) {
+                Column(
+                    modifier = Modifier
+                        .clip(RoundedCornerShape(25.dp))
+                        .background(Color.Red)
+                        .padding(10.dp).fillMaxWidth(), verticalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        text = "Я купил энергетик",
+                        fontSize = 20.sp,
+                        color = MaterialTheme.colorScheme.onBackground,
+                        fontWeight = FontWeight.Medium,
+                        modifier = Modifier.padding(16.dp).fillMaxWidth(),
+                        textAlign = TextAlign.Center
+                    )
 
+                }
             }
-        }
-        Box(modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp).clickable {  }){
-            Column(
-                modifier = Modifier
-                    .clip(RoundedCornerShape(25.dp))
-                    .background(Color.Green)
-                    .padding(10.dp).fillMaxWidth(), verticalArrangement = Arrangement.Center
-            ) {
-                Text(text = "Я справился с соблазном",
-                    fontSize = 20.sp,
-                    color = MaterialTheme.colorScheme.onBackground,
-                    fontWeight = FontWeight.Medium,
-                    modifier = Modifier.padding(16.dp).fillMaxWidth(),
-                    textAlign = TextAlign.Center)
+            Box(modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp).clickable { }) {
+                Column(
+                    modifier = Modifier
+                        .clip(RoundedCornerShape(25.dp))
+                        .background(Color.Green)
+                        .padding(10.dp).fillMaxWidth(), verticalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        text = "Я справился с соблазном",
+                        fontSize = 20.sp,
+                        color = MaterialTheme.colorScheme.onBackground,
+                        fontWeight = FontWeight.Medium,
+                        modifier = Modifier.padding(16.dp).fillMaxWidth(),
+                        textAlign = TextAlign.Center
+                    )
 
+                }
             }
         }
     }
