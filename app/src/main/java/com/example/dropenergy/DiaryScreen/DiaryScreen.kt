1364b7e4c85ccf946de327f13720a8c2f9045781
Leturgone <finekon221@gmail.com>
2024-11-02 21:05:02 +0300

    Сделано центрирование в DiaryScreen

--- app/src/main/java/com/example/dropenergy/DiaryScreen/DiaryScreen.kt
+++ app/src/main/java/com/example/dropenergy/DiaryScreen/DiaryScreen.kt
@@ -1,8 +1,10 @@
 package com.example.dropenergy.DiaryScreen
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -12,6 +14,7 @@ import androidx.compose.material.icons.rounded.Circle
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -40,7 +43,9 @@ var diaryList = listOf(
 @Preview
 @Composable
 fun DiaryScreen(){
-    Column {
+    Box(
+        modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center
+    ) {
         LazyColumn(){
             items(diaryList.size){
                 val record = diaryList[it]
