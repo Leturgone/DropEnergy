c7001540a91962d48b2a9826986e0d7648b1d8ed
Leturgone <finekon221@gmail.com>
2024-11-02 21:27:25 +0300

    Создан файл для экрана c прогнозами по деньгам

---
+++ app/src/main/java/com/example/dropenergy/ProgressScreen/CanScreen.kt
@@ -0,0 +1,8 @@
+package com.example.dropenergy.ProgressScreen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun CanScreen(){}
