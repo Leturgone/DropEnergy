83bc0e59c590b74a5d9be048cb79ad6f509a6da4
Leturgone <finekon221@gmail.com>
2024-11-05 13:41:13 +0300

    Сделана заготовка для интерфейса прогнозов по деньгам

--- app/src/main/java/com/example/dropenergy/ProgressScreen/MoneyScreen.kt
+++ app/src/main/java/com/example/dropenergy/ProgressScreen/MoneyScreen.kt
@@ -1,7 +1,15 @@
 package com.example.dropenergy.ProgressScreen
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Preview
 @Composable
@@ -12,4 +20,41 @@ fun MoneyScreen(){
     var in_mounth_money = 0
     var in_year_money = 0
 
+    Column {
+        Text(text = "Не выпито банок $ekonom_money",
+            fontSize = 24.sp,
+            color = MaterialTheme.colorScheme.onBackground,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(16.dp))
+
+        Text(text = "Прогноз",
+            fontSize = 24.sp,
+            color = MaterialTheme.colorScheme.onBackground,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(16.dp))
+
+        Text(text = "Не выпито в день $in_day_money",
+            fontSize = 24.sp,
+            color = MaterialTheme.colorScheme.onBackground,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(16.dp))
+
+        Text(text = "Не выпито в неделю $in_week_money",
+            fontSize = 24.sp,
+            color = MaterialTheme.colorScheme.onBackground,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(16.dp))
+
+        Text(text = "Не выпито в месяц $in_mounth_money",
+            fontSize = 24.sp,
+            color = MaterialTheme.colorScheme.onBackground,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(16.dp))
+
+        Text(text = "Не выпито в год $in_year_money",
+            fontSize = 24.sp,
+            color = MaterialTheme.colorScheme.onBackground,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(16.dp))
+    }
 }
