2ddebe7cfbd9f77e907e5f8c6fbc659f3f28defe
Leturgone <finekon221@gmail.com>
2024-11-06 14:13:40 +0300

    Изменено положение элементов в CanScreen

--- app/src/main/java/com/example/dropenergy/ProgressScreen/CanScreen.kt
+++ app/src/main/java/com/example/dropenergy/ProgressScreen/CanScreen.kt
@@ -1,10 +1,13 @@
 package com.example.dropenergy.ProgressScreen
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -20,11 +23,25 @@ fun CanScreen(){
     var in_mounth_can = 0
     var in_year_can = 0
     Column {
-        Text(text = "Не выпито банок $ekonom_can",
-            fontSize = 24.sp,
-            color = MaterialTheme.colorScheme.onBackground,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(16.dp))
+        Box(Modifier.fillMaxWidth(),contentAlignment = Alignment.Center) {
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Text(
+                    text = "Не выпито",
+                    fontSize = 28.sp,
+                    color = MaterialTheme.colorScheme.onBackground,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(16.dp)
+                )
+                Text(
+                    text = "$ekonom_can банок",
+                    fontSize = 24.sp,
+                    color = MaterialTheme.colorScheme.onBackground,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+
+        }
 
         Text(text = "Прогноз",
             fontSize = 24.sp,
@@ -32,28 +49,31 @@ fun CanScreen(){
             fontWeight = FontWeight.Bold,
             modifier = Modifier.padding(16.dp))
 
-        Text(text = "Не выпито в день $in_day_can",
-            fontSize = 24.sp,
-            color = MaterialTheme.colorScheme.onBackground,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(16.dp))
+        Column(Modifier.padding(start = 16.dp)) {
+            Text(text = "$in_day_can банок в день",
+                fontSize = 24.sp,
+                color = MaterialTheme.colorScheme.onBackground,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(16.dp))
 
-        Text(text = "Не выпито в неделю $in_week_can",
-            fontSize = 24.sp,
-            color = MaterialTheme.colorScheme.onBackground,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(16.dp))
+            Text(text = "$in_week_can банок в неделю ",
+                fontSize = 24.sp,
+                color = MaterialTheme.colorScheme.onBackground,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(16.dp))
 
-        Text(text = "Не выпито в месяц $in_mounth_can",
-            fontSize = 24.sp,
-            color = MaterialTheme.colorScheme.onBackground,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(16.dp))
+            Text(text = "$in_mounth_can банок в месяц ",
+                fontSize = 24.sp,
+                color = MaterialTheme.colorScheme.onBackground,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(16.dp))
 
-        Text(text = "Не выпито в год $in_year_can",
-            fontSize = 24.sp,
-            color = MaterialTheme.colorScheme.onBackground,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(16.dp))
+            Text(text = "$in_year_can банок год ",
+                fontSize = 24.sp,
+                color = MaterialTheme.colorScheme.onBackground,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(16.dp))
+        }
+
     }
 }
