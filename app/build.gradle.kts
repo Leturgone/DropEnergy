f7b2919206dde73100639977d894fa91ee6e6978
Leturgone <finekon221@gmail.com>
2024-11-14 17:07:32 +0300

    Добавлена зависимость для UI тестов

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,4 +72,7 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.8.3")
     implementation("com.maxkeppeler.sheets-compose-dialogs:calendar:1.0.2")
 
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:$version")
+
+
 }
