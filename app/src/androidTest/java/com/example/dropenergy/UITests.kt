dbbe3ecbc2323daade70117b8b2bb842fa144544
Leturgone <finekon221@gmail.com>
2024-11-14 17:35:28 +0300

    Начато тестирование

--- app/src/androidTest/java/com/example/dropenergy/UITests.kt
+++ app/src/androidTest/java/com/example/dropenergy/UITests.kt
@@ -1,5 +1,6 @@
 package com.example.dropenergy
 
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
@@ -7,10 +8,15 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * UI тесты
  */
+
+@get:Rule
+val composeTestRule = createAndroidComposeRule<MainActivity>()
+
 @RunWith(AndroidJUnit4::class)
 class UITests {
     @Test
