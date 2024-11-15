2d01c59d48ea630eace8dde02dafa788ee1a8ef6
Leturgone <finekon221@gmail.com>
2024-11-15 16:58:12 +0300

    Сделан тест для навигации с боттом баром

--- app/src/androidTest/java/com/example/dropenergy/UITests.kt
+++ app/src/androidTest/java/com/example/dropenergy/UITests.kt
@@ -1,6 +1,17 @@
 package com.example.dropenergy
 
+import androidx.compose.ui.test.hasClickAction
+import androidx.compose.ui.test.hasContentDescription
+import androidx.compose.ui.test.hasNoClickAction
+import androidx.compose.ui.test.hasSetTextAction
+import androidx.compose.ui.test.hasTestTag
+import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.onRoot
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.printToLog
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
@@ -14,15 +25,51 @@ import org.junit.Rule
  * UI тесты
  */
 
-@get:Rule
-val composeTestRule = createAndroidComposeRule<MainActivity>()
+
 
 @RunWith(AndroidJUnit4::class)
 class UITests {
+
+
+
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun testBottomBarNavigation(){
+        composeTestRule.setContent { MainScreen() }
+        composeTestRule.onRoot().printToLog("MY TAG")
+        composeTestRule.onNode(BottomBar.bottomBarDiaryButton).performClick()
+        composeTestRule.onRoot().printToLog("MY TAG")
+        composeTestRule.onNode(DiaryScreen.ScreenTemplate).assertExists()
+
+        composeTestRule.onNode((BottomBar.bottomBarStatButton)).performClick()
+        composeTestRule.onNode(StatScreen.ScreenDayCecTemplate).assertExists()
+
+        composeTestRule.onNode(BottomBar.bottomBarAddButton).performClick()
+        composeTestRule.onRoot().printToLog("MY TAG")
+
+        composeTestRule.onNode(AddNewRecordScreen.ScreenTemlate).assertExists()
+        composeTestRule.onNode(AddNewRecordScreen.NewGoodRecBtn).assertExists()
+        composeTestRule.onNode(AddNewRecordScreen.NewBuyRecBtn).assertExists()
+        composeTestRule.onNode(AddNewRecordScreen.NewWantRecBtn).assertExists()
+
+    }
+    fun testProgressScreenNavigation(){
+        composeTestRule.setContent { MainScreen() }
+        composeTestRule.onNode(StatScreen.DaysSection).assertExists()
+        composeTestRule.onNode(StatScreen.ScreenProgressTemplate).assertExists()
+        composeTestRule.onNode(StatScreen.MoneySec).assertExists()
+        composeTestRule.onNode(StatScreen.CanSec).assertExists().assertExists()
+    }
+
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.dropenergy", appContext.packageName)
+
     }
 }
