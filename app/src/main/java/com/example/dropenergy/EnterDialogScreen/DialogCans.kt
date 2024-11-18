259d8f1384679356f1482bdbd487bed3c898de0e
Leturgone <finekon221@gmail.com>
2024-11-18 21:04:28 +0300

    Сделан начальный дизайн

--- app/src/main/java/com/example/dropenergy/EnterDialogScreen/DialogCans.kt
+++ app/src/main/java/com/example/dropenergy/EnterDialogScreen/DialogCans.kt
@@ -7,15 +7,22 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.displayCutoutPadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -23,26 +30,37 @@ import androidx.compose.ui.unit.sp
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun AskMoneyCansScreen(){
+    var inputText  = remember {mutableStateOf(" ")}
     Column {
         Box(Modifier.fillMaxWidth(),contentAlignment = Alignment.Center) {
             Column( modifier = Modifier.fillMaxSize(),
                 horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.SpaceBetween) {
-                Column(horizontalAlignment = Alignment.CenterHorizontally,verticalArrangement = Arrangement.SpaceBetween) {
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
                     Text(
                         text = "Сколько вы покупаете энергетиков в день?",
                         fontSize = 28.sp,
                         color = MaterialTheme.colorScheme.onBackground,
                         fontWeight = FontWeight.Bold,
-                        modifier = Modifier.padding(16.dp)
+                        modifier = Modifier.padding(16.dp),
+                        textAlign = TextAlign.Center
                     )
+                    Spacer(modifier = Modifier.height(60.dp))
                     TextField(
                         value = "Количество энергетиков",
-                        onValueChange = {})
+                        keyboardOptions =  KeyboardOptions(keyboardType = KeyboardType.Number),
+                        onValueChange = {
+                            inputText.value = it
+                        })
                 }
-                Button(onClick = { /*TODO*/ }) {
+                Button(onClick = {
+
+                //Загрузка в бд
+
+                }) {
                     Text(text = "Дальше")
                     
                 }
+                Spacer(modifier = Modifier.height(1.dp))
             }
         }
     }
