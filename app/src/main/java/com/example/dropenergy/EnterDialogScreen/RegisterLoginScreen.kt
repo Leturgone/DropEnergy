a7d2816c80c94f532fb1515825515965c699e50c
Leturgone <finekon221@gmail.com>
2024-11-19 14:40:13 +0300

    Сделано скрытие пароля

--- app/src/main/java/com/example/dropenergy/EnterDialogScreen/RegisterLoginScreen.kt
+++ app/src/main/java/com/example/dropenergy/EnterDialogScreen/RegisterLoginScreen.kt
@@ -9,17 +9,26 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -31,6 +40,8 @@ import androidx.navigation.NavHostController
 fun LoginRegScreen(navController: NavHostController){
     var loginInputText  = remember { mutableStateOf(" ") }
     var passwordInputText  = remember { mutableStateOf(" ") }
+    var passwordVisible by remember { mutableStateOf(false) }
+
     Column {
         Box(Modifier.fillMaxWidth(),contentAlignment = Alignment.Center) {
             Column( modifier = Modifier.fillMaxSize(),
@@ -57,10 +68,24 @@ fun LoginRegScreen(navController: NavHostController){
                     Spacer(modifier = Modifier.height(60.dp))
                     TextField(
                         value = passwordInputText.value,
+                        singleLine = true,
                         label = { Text("Пароль") },
+                        visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
                         keyboardOptions =  KeyboardOptions(keyboardType = KeyboardType.Password),
                         onValueChange = {
                             passwordInputText.value = it
+                        },
+                        trailingIcon = {
+                            val image = if (passwordVisible)
+                                Icons.Filled.Visibility
+                            else Icons.Filled.VisibilityOff
+
+                            val description = if (passwordVisible) "Hide password" else "Show password"
+
+                            IconButton(onClick = {passwordVisible = !passwordVisible}){
+                                Icon(imageVector  = image, description)
+                            }
+
                         })
                 }
                 Button(onClick = {
