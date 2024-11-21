4c15f44ded7b82c233896967c5bf409f57f57a59
Leturgone <finekon221@gmail.com>
2024-11-21 22:00:27 +0300

    Сделано измненение цвета кнопки при вводе

--- app/src/main/java/com/example/dropenergy/EnterDialogScreen/DialogCansScreen.kt
+++ app/src/main/java/com/example/dropenergy/EnterDialogScreen/DialogCansScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -22,6 +23,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
@@ -29,11 +31,13 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.example.dropenergy.ui.theme.Purple80
 
 //@Preview(showBackground = true)
 @Composable
 fun AskCansScreen(navController: NavHostController){
     var inputText  by remember {mutableStateOf("")}
+    var buttonColor by remember { mutableStateOf(Purple80) }
     Column {
         LinearProgressIndicator(
             progress = 2/3.toFloat(),
@@ -59,6 +63,7 @@ fun AskCansScreen(navController: NavHostController){
                         keyboardOptions =  KeyboardOptions(keyboardType = KeyboardType.Number),
                         onValueChange = {
                             inputText = it
+                            buttonColor = Color.Green
                         })
                 }
                 Button(onClick = {
@@ -66,7 +71,9 @@ fun AskCansScreen(navController: NavHostController){
 
                 //Загрузка в бд
 
-                }) {
+                },
+                    colors = ButtonDefaults.buttonColors(containerColor = buttonColor)
+                ) {
                     Text(text = "Дальше")
                     
                 }
