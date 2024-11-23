3c10ba5a66987211b62454da02791b8e658eee73
Leturgone <finekon221@gmail.com>
2024-11-23 19:54:24 +0300

    Сделаны поля

--- app/src/main/java/com/example/dropenergy/database/User.kt
+++ app/src/main/java/com/example/dropenergy/database/User.kt
@@ -1,4 +1,14 @@
 package com.example.dropenergy.database
 
-class User {
-}
+import com.example.dropenergy.data.CheckDay
+import com.example.dropenergy.data.DiaryRecord
+
+data class User(
+    var login: String,
+    var password: String,
+    var energy_money: Int,
+    var currency: String,
+    var diary: MutableList<DiaryRecord>,
+    var week: List<CheckDay>
+)
+
