390a654db1918832b93b71272d358f3b914c5e33
Leturgone <finekon221@gmail.com>
2024-11-28 00:17:42 +0300

    Добавлена вью модель

--- app/src/main/java/com/example/dropenergy/di/MainApplication.kt
+++ app/src/main/java/com/example/dropenergy/di/MainApplication.kt
@@ -1,25 +1,31 @@
-package com.example.dropenergy.database.di
+package com.example.dropenergy.di
 
 import android.app.Application
 import com.example.dropenergy.database.repository.AuthRepository
 import com.example.dropenergy.database.repository.IAuthRepository
 import com.example.dropenergy.database.viewModel.AuthViewModel
+import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
 import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.context.startKoin
 import org.koin.dsl.module
+import org.koin.core.logger.Level
+
+
+val appModule = module {
+    single { FirebaseAuth.getInstance() }
+    single<IAuthRepository> { AuthRepository(get()) }
+    viewModel { AuthViewModel(get()) }
+}
 
 class MainApplication : Application() {
 
-    private val appModule = module {
-        single { FirebaseAuth.getInstance() }
-        single<IAuthRepository> { AuthRepository(get()) }
-        viewModel { AuthViewModel(get()) }
-    }
     override fun onCreate() {
         super.onCreate()
         startKoin {
+            androidLogger(Level.DEBUG)
             androidContext(this@MainApplication)
             modules(appModule)
         }
