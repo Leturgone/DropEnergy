0d20b9e9d7a9adc0ae22993ac71d871448926a1d
Leturgone <finekon221@gmail.com>
2024-11-27 19:08:06 +0300

    Сделана вьюмодель для аутентификации

---
+++ app/src/main/java/com/example/dropenergy/database/viewModel/AuthViewModel.kt
@@ -0,0 +1,46 @@
+package com.example.dropenergy.database.viewModel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.dropenergy.database.repository.IAuthRepository
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+class AuthViewModel(
+    private val repository: IAuthRepository
+) : ViewModel() {
+
+    private val _loginFlow = MutableStateFlow<FirebaseUser?>(null)
+
+    val loginFlow: StateFlow<FirebaseUser?> = _loginFlow
+
+    private val _signupFlow = MutableStateFlow<FirebaseUser?>(null)
+
+    val signupFlow: StateFlow<FirebaseUser?> = _signupFlow
+
+    val currentUser = repository.getCurrentUser()
+
+    init {
+        if (repository.getCurrentUser() != null){
+            _loginFlow.value = repository.getCurrentUser()
+        }
+    }
+
+    fun login(email: String, password: String) = viewModelScope.launch {
+        val result = repository.login(email, password)
+        _loginFlow.value = result
+    }
+
+    fun signup(email: String, password: String) = viewModelScope.launch {
+        val result = repository.signup(email, password)
+        _signupFlow.value = result
+    }
+
+    fun logout(){
+        repository.logout()
+        _loginFlow.value = null
+        _signupFlow.value = null
+    }
+}
