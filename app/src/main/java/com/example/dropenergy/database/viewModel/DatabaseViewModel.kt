7c5874f8662f60e0e631875e0978b5017e9c9e8f
Leturgone <finekon221@gmail.com>
2024-11-28 00:18:06 +0300

    закоменчено на будущее

--- app/src/main/java/com/example/dropenergy/database/viewModel/DatabaseViewModel.kt
+++ app/src/main/java/com/example/dropenergy/database/viewModel/DatabaseViewModel.kt
@@ -1,13 +1,13 @@
 package com.example.dropenergy.database.viewModel
 
-import androidx.lifecycle.ViewModel
-import com.example.dropenergy.database.model.User
-import com.example.dropenergy.database.repository.IUserRepository
+//import androidx.lifecycle.ViewModel
+//import com.example.dropenergy.database.model.User
+//import com.example.dropenergy.database.repository.IUserRepository
 
-class DatabaseViewModel(
-    private val repository: IUserRepository)
-    : ViewModel() {
-//    fun getUsers(): List<User> {
-//        return repository.getUsers()
-//    }
-}
+//class DatabaseViewModel(
+//    private val repository: IUserRepository)
+//    : ViewModel() {
+////    fun getUsers(): List<User> {
+////        return repository.getUsers()
+////    }
+//}
