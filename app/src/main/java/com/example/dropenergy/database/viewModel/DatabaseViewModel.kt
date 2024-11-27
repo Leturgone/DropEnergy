3093fc4a27cef1b588f7782ed8f2217ea04d648f
Leturgone <finekon221@gmail.com>
2024-11-27 14:01:54 +0300

    Создана вью модель

---
+++ app/src/main/java/com/example/dropenergy/database/viewModel/DatabaseViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.dropenergy.database.viewModel
+
+import androidx.lifecycle.ViewModel
+import com.example.dropenergy.database.model.User
+import com.example.dropenergy.database.repository.IUserRepository
+
+class DatabaseViewModel(
+    private val repository: IUserRepository)
+    : ViewModel() {
+    fun getUsers(): List<User> {
+        return repository.getUsers()
+    }
+}
