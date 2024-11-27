ce4672e0dacb33dffd6e569ac61b1f7310ce7ee4
Leturgone <finekon221@gmail.com>
2024-11-27 19:08:38 +0300

    Сделан интерфейс репозитория

---
+++ app/src/main/java/com/example/dropenergy/database/repository/IAuthRepository.kt
@@ -0,0 +1,15 @@
+package com.example.dropenergy.database.repository
+
+import com.google.firebase.auth.FirebaseUser
+
+interface IAuthRepository {
+
+
+    suspend fun login(email: String, password: String): FirebaseUser?
+    suspend fun signup(email: String, password: String): FirebaseUser?
+
+    fun logout()
+
+    fun getCurrentUser(): FirebaseUser?
+
+}
