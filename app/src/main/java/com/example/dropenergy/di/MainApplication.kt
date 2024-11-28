85e9f45a8dd6351e5cfcc2b349e513f0a74b1c32
Leturgone <finekon221@gmail.com>
2024-11-28 14:58:27 +0300

    Обновлен DI

--- app/src/main/java/com/example/dropenergy/di/MainApplication.kt
+++ app/src/main/java/com/example/dropenergy/di/MainApplication.kt
@@ -3,9 +3,13 @@ package com.example.dropenergy.di
 import android.app.Application
 import com.example.dropenergy.database.repository.AuthRepository
 import com.example.dropenergy.database.repository.IAuthRepository
+import com.example.dropenergy.database.repository.IUserRepository
+import com.example.dropenergy.database.repository.UserRepository
 import com.example.dropenergy.database.viewModel.AuthViewModel
 import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.androidx.viewmodel.dsl.viewModel
@@ -16,8 +20,11 @@ import org.koin.core.logger.Level
 
 val appModule = module {
     single { FirebaseAuth.getInstance() }
+    single { Firebase.database.reference }
+    single<IUserRepository> {UserRepository(get())}
     single<IAuthRepository> { AuthRepository(get()) }
     viewModel { AuthViewModel(get()) }
+
 }
 
 class MainApplication : Application() {
