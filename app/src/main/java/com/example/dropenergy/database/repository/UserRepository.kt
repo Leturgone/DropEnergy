c1c5a477a9e95011eeb112e2d1f7db20756cff84
Leturgone <finekon221@gmail.com>
2024-11-28 14:58:17 +0300

    Начата реализация репозитория

--- app/src/main/java/com/example/dropenergy/database/repository/UserRepository.kt
+++ app/src/main/java/com/example/dropenergy/database/repository/UserRepository.kt
@@ -1,7 +1,28 @@
 package com.example.dropenergy.database.repository
 
+import com.example.dropenergy.data.CheckDay
+import com.example.dropenergy.data.DiaryRecord
 import com.example.dropenergy.database.model.User
+import com.google.firebase.database.DatabaseReference
 
-class UserRepository : IUserRepository{
+class UserRepository(
+    private val database: DatabaseReference
+) : IUserRepository{
+    override suspend fun writeUser(user: User) {
+        database.child("users").child(user.Uid).setValue(user)
+    }
+
+    override suspend fun getUser(user: User) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun updateDiary(diaryRecord: DiaryRecord) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun updateWeek(newDay: CheckDay) {
+        TODO("Not yet implemented")
+    }
+
 
 }
