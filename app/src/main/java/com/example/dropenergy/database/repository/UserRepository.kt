181b6cf646c465c90c19b4a4ce82269fb55e8613
Leturgone <finekon221@gmail.com>
2024-11-28 23:00:36 +0300

    Добавлена функция обновления данных

--- app/src/main/java/com/example/dropenergy/database/repository/UserRepository.kt
+++ app/src/main/java/com/example/dropenergy/database/repository/UserRepository.kt
@@ -9,21 +9,37 @@ import com.google.firebase.database.DatabaseReference
 class UserRepository(
     private val database: DatabaseReference
 ) : IUserRepository{
-    override suspend fun writeUser(user: User) {
-        database.child("users").child(user.Uid).setValue(user)
+    override suspend fun writeUser(uid: String,user: User) {
+        database.child("users").child(uid).setValue(user)
         Log.i("Firebase","Пользователь загружен в БД")
     }
 
-    override suspend fun getUser(user: User) {
+    override suspend fun getUser(uid:String): User? {
         Log.i("Firebase","Данные пользователя получены из БД")
+        return null
         TODO("Not yet implemented")
     }
 
-    override suspend fun updateDiary(diaryRecord: DiaryRecord) {
+    override suspend fun updateUser(
+        uid: String,
+        energy_count: Int,
+        energy_money: Int,
+        currency: String,
+        diary: Map<Int, DiaryRecord>,
+        week: List<CheckDay>
+    ) {
+        database.child("users").child(uid).child("energy_count").setValue(energy_count)
+        database.child("users").child(uid).child("energy_money").setValue(energy_money)
+        database.child("users").child(uid).child("currency").setValue(currency)
+        database.child("users").child(uid).child("diary").setValue(diary)
+        database.child("users").child(uid).child("week").setValue(week)
+    }
+
+    override suspend fun updateDiary(uid: String,diaryRecord: DiaryRecord) {
         TODO("Not yet implemented")
     }
 
-    override suspend fun updateWeek(newDay: CheckDay) {
+    override suspend fun updateWeek(uid: String,newDay: CheckDay) {
         TODO("Not yet implemented")
     }
 
