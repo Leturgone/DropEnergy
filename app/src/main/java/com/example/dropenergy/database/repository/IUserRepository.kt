433674daa7644e9970c34534f394a74634eefe5c
Leturgone <finekon221@gmail.com>
2024-11-28 14:57:47 +0300

    Создан интерфейс для репозитория по работе с юзерами

--- app/src/main/java/com/example/dropenergy/database/repository/IUserRepository.kt
+++ app/src/main/java/com/example/dropenergy/database/repository/IUserRepository.kt
@@ -1,7 +1,16 @@
 package com.example.dropenergy.database.repository
 
+import com.example.dropenergy.data.CheckDay
+import com.example.dropenergy.data.DiaryRecord
 import com.example.dropenergy.database.model.User
 
 interface IUserRepository {
+    suspend fun writeUser(user: User)
+
+    suspend fun getUser(user: User)
+
+    suspend fun updateDiary(diaryRecord: DiaryRecord)
+
+    suspend fun updateWeek(newDay: CheckDay)
 
 }
