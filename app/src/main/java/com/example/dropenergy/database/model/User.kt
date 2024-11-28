0bce6dd55e0b8715cc84e7687245a8de0dbec7b9
Leturgone <finekon221@gmail.com>
2024-11-28 23:00:13 +0300

    Добавлено поле для каунта

--- app/src/main/java/com/example/dropenergy/database/model/User.kt
+++ app/src/main/java/com/example/dropenergy/database/model/User.kt
@@ -4,9 +4,9 @@ import com.example.dropenergy.data.CheckDay
 import com.example.dropenergy.data.DiaryRecord
 
 data class User(
-    val Uid: String,
     val login: String,
     val password: String,
+    val energy_count: Int?,
     val energy_money: Int?,
     val currency: String?,
     var diary: Map<String,DiaryRecord>,
