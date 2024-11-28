57ec069c68d97471ab0af0a69995f74dba66dd3e
Leturgone <finekon221@gmail.com>
2024-11-28 14:58:02 +0300

    Обновлен класс юзера

--- app/src/main/java/com/example/dropenergy/database/model/User.kt
+++ app/src/main/java/com/example/dropenergy/database/model/User.kt
@@ -4,11 +4,12 @@ import com.example.dropenergy.data.CheckDay
 import com.example.dropenergy.data.DiaryRecord
 
 data class User(
+    val Uid: String,
     val login: String,
     val password: String,
     val energy_money: Int,
     val currency: String,
-    var diary: MutableList<DiaryRecord>,
+    var diary: Map<String,DiaryRecord>,
     var week: List<CheckDay>
 )
 
