066e68881dc1c9cca06d2b31804a46a11c050dcb
Leturgone <finekon221@gmail.com>
2024-11-29 21:58:03 +0300

    Изменены параметры

--- app/src/main/java/com/example/dropenergy/database/model/User.kt
+++ app/src/main/java/com/example/dropenergy/database/model/User.kt
@@ -6,9 +6,9 @@ import com.example.dropenergy.data.DiaryRecord
 data class User(
     val login: String,
     val password: String,
-    val energy_count: Int?,
-    val energy_money: Int?,
-    val currency: String?,
+    var energy_count: Int?,
+    var energy_money: Int?,
+    var currency: String?,
     var diary: Map<String,DiaryRecord>,
     var week: List<CheckDay>
 )
