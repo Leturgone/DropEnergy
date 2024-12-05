8f3c2468a4dc5faaa8fc89ac44e8e47aedc40770
Leturgone <finekon221@gmail.com>
2024-12-05 19:41:08 +0300

    Исправлена ошибка в нейминге

--- app/src/main/java/com/example/dropenergy/database/repository/LoginRegState.kt
+++ app/src/main/java/com/example/dropenergy/database/repository/LoginRegState.kt
@@ -2,6 +2,7 @@ package com.example.dropenergy.database.repository
 
 sealed class LoginRegState<out R> {
     data class Success<out R>(val result: R): LoginRegState<R>()
+
     data class Failure(val exception: Exception) : LoginRegState<Nothing>()
     data object Loading : LoginRegState<Nothing>()
 }
