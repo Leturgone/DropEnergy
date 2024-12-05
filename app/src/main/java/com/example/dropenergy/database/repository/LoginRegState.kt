e6dc2269ffcf4e8c0a18abeaa9c73527f370a719
Leturgone <finekon221@gmail.com>
2024-12-05 18:23:07 +0300

    логин с состоянием строковым

--- app/src/main/java/com/example/dropenergy/database/repository/LoginRegState.kt
+++ app/src/main/java/com/example/dropenergy/database/repository/LoginRegState.kt
@@ -1,7 +1,7 @@
 package com.example.dropenergy.database.repository
 
 sealed class LoginRegState<out R> {
-    data class Sucsess<out R>(val result: R): LoginRegState<R>()
+    class Sucsess<out R>(val result: R): LoginRegState<R>()
     data class Failure(val exception: Exception) : LoginRegState<Nothing>()
     data object Loading : LoginRegState<Nothing>()
 }
