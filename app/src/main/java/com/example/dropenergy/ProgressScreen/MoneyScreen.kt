951f41d20b337aae5597c582fc9b131aa3bf27b6
Leturgone <finekon221@gmail.com>
2024-12-19 13:07:42 +0300

    Добавлено отслеживание сохр денег

--- app/src/main/java/com/example/dropenergy/ProgressScreen/MoneyScreen.kt
+++ app/src/main/java/com/example/dropenergy/ProgressScreen/MoneyScreen.kt
@@ -1,5 +1,7 @@
 package com.example.dropenergy.ProgressScreen
 
+import android.annotation.SuppressLint
+import android.widget.Toast
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -7,25 +9,50 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.dropenergy.database.repository.GetDBState
 import com.example.dropenergy.database.viewModel.DBViewModel
 
 //@Preview
+@SuppressLint("StateFlowValueCalledInComposition")
 @Composable
 fun MoneyScreen(viewModel: DBViewModel){
 
     //Подгрузка из БД
-    var ekonom_money = 0
-    var in_day_money = 0
-    var in_week_money = 0
-    var in_mounth_money = 0
-    var in_year_money = 0
+    var ekonom_money by remember { mutableIntStateOf(0) }
+    var in_day_money by remember { mutableIntStateOf(0) }
+    var in_week_money by remember { mutableIntStateOf(0) }
+    var in_mounth_money by remember { mutableIntStateOf(0) }
+    var in_year_money by remember { mutableIntStateOf(0) }
 
+    val ctx = LocalContext.current
+    LaunchedEffect(Unit) {
+        viewModel.getSavedMoney()
+    }
+
+    viewModel.savedMoneyFlow.collectAsState().value.let {state ->
+        when(state){
+            is GetDBState.Success -> {
+                ekonom_money = state.result
+            }
+            is GetDBState.Loading -> Toast.makeText(ctx,"Загрузка сохр денег", Toast.LENGTH_SHORT).show()
+            is GetDBState.Failure -> Toast.makeText(ctx,"Ошибка сохр денег", Toast.LENGTH_SHORT).show()
+            else -> {null}
+        }
+    }
     Column {
         Box(Modifier.fillMaxWidth(),contentAlignment = Alignment.Center) {
             Column(horizontalAlignment = Alignment.CenterHorizontally) {
