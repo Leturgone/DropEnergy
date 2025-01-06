3fb2f7c4c689378542a652013a72e2f47636f786
Leturgone <finekon221@gmail.com>
2025-01-06 18:11:16 +0300

    Сделана кнопка выхода из аккаунта

--- app/src/main/java/com/example/dropenergy/database/OptionsScreen/OptionsScreen.kt
+++ app/src/main/java/com/example/dropenergy/database/OptionsScreen/OptionsScreen.kt
@@ -1,12 +1,8 @@
 package com.example.dropenergy.database.OptionsScreen
 
-import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.displayCutoutPadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -14,31 +10,29 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
 import com.example.dropenergy.data.OptionsFunction
+import com.example.dropenergy.database.viewModel.DBViewModel
 
 
-fun funnyFun(){
-    Log.i("Options","FUNNY")
-}
-@Preview(showBackground = true)
 @Composable
-fun OptionsScreen(){
+fun OptionsScreen(navController: NavHostController, viewModel: DBViewModel){
     val optList = listOf(
         OptionsFunction(
             title = "Выйти из аккаунта"
-        ) { funnyFun() }
+        ) { viewModel.logout()
+            navController.popBackStack()
+            navController.popBackStack()
+            navController.popBackStack()
+            navController.navigate("reg")
+        }
 
     )
-
-
-    val ctx = LocalContext.current
 
     Column() {
         Text(text = "Настройки",
