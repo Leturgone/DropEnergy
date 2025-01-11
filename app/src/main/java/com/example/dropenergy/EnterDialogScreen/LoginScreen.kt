6354c160512b41c14255f4e623340079cc115dd3
Leturgone <finekon221@gmail.com>
2025-01-11 19:16:13 +0300

    Введены ресурсы для LoginScreen

--- app/src/main/java/com/example/dropenergy/EnterDialogScreen/LoginScreen.kt
+++ app/src/main/java/com/example/dropenergy/EnterDialogScreen/LoginScreen.kt
@@ -33,6 +33,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -44,8 +45,9 @@ import androidx.navigation.NavHostController
 import com.example.dropenergy.database.repository.GetDBState
 import com.example.dropenergy.database.viewModel.DBViewModel
 import com.example.dropenergy.ui.theme.Purple80
+import com.example.dropenergy.R
 
-//@Preview(showBackground = true)
+
 @Composable
 fun LoginScreen(navController: NavHostController, viewModel: DBViewModel?){
     var loginInputText  by remember { mutableStateOf("") }
@@ -56,6 +58,10 @@ fun LoginScreen(navController: NavHostController, viewModel: DBViewModel?){
     val ctx = LocalContext.current
 
     val loginState = viewModel?.loginFlow?.collectAsState()
+
+    val emailErr = stringResource(id = R.string.email_err)
+    val passwordErr = stringResource(id = R.string.password_err)
+    val shortPasswordErr = stringResource(id = R.string.password_short_err)
 
     Column {
         LinearProgressIndicator(
@@ -71,7 +77,7 @@ fun LoginScreen(navController: NavHostController, viewModel: DBViewModel?){
                 Column(horizontalAlignment = Alignment.CenterHorizontally) {
                     Spacer(modifier = Modifier.height(60.dp))
                     Text(
-                        text = "Войти",
+                        text = stringResource(id = R.string.login),
                         fontSize = 28.sp,
                         color = MaterialTheme.colorScheme.onBackground,
                         fontWeight = FontWeight.Bold,
@@ -82,22 +88,19 @@ fun LoginScreen(navController: NavHostController, viewModel: DBViewModel?){
 
                     OutlinedTextField(
                         value = loginInputText,
-                        label = { Text("Логин") },
+                        label = { Text(stringResource(id = R.string.email)) },
                         singleLine = true,
                         keyboardOptions =  KeyboardOptions(keyboardType = KeyboardType.Email),
                         onValueChange = {
                             loginInputText = it
                             loginOK = true
                         })
-
-
-
                     Spacer(modifier = Modifier.height(60.dp))
 
                     OutlinedTextField(
                         value = passwordInputText,
                         singleLine = true,
-                        label = { Text("Пароль") },
+                        label = { Text(stringResource(id = R.string.password)) },
                         visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
                         keyboardOptions =  KeyboardOptions(keyboardType = KeyboardType.Password),
                         onValueChange = {
@@ -134,7 +137,8 @@ fun LoginScreen(navController: NavHostController, viewModel: DBViewModel?){
                         is GetDBState.Loading -> {
                             CircularProgressIndicator()
                         }
-                        is GetDBState.Failure -> Toast.makeText(ctx,"Неверный логин или пароль", Toast.LENGTH_SHORT).show()
+                        is GetDBState.Failure -> Toast.makeText(ctx, stringResource(id = R.string.login_err),
+                            Toast.LENGTH_SHORT).show()
                         else -> {null}
                     }
 
@@ -143,14 +147,14 @@ fun LoginScreen(navController: NavHostController, viewModel: DBViewModel?){
                 Button(onClick = {
                     if( loginInputText.isEmpty() || !loginInputText.matches("^[A-Za-z0-9@.]+$".toRegex())
                         || loginInputText.matches("\\s".toRegex())){
-                        Toast.makeText(ctx,"Проверьте логин на ошибки", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(ctx, emailErr, Toast.LENGTH_SHORT).show()
                     }
                     else if( passwordInputText.isEmpty() || !passwordInputText.matches("^[A-Za-z0-9]+$".toRegex())
                         || passwordInputText.matches("\\s".toRegex())){
-                        Toast.makeText(ctx,"Пароль не должен содержать пробелов", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(ctx, passwordErr, Toast.LENGTH_SHORT).show()
                     }
                     else if (passwordInputText.length < 8){
-                        Toast.makeText(ctx,"Пароль должен содержать минимум 8 символов",Toast.LENGTH_SHORT).show()
+                        Toast.makeText(ctx, shortPasswordErr,Toast.LENGTH_SHORT).show()
                     }
                     else {
                         viewModel?.login(loginInputText,passwordInputText)
@@ -159,12 +163,8 @@ fun LoginScreen(navController: NavHostController, viewModel: DBViewModel?){
                 },
                     colors = ButtonDefaults.buttonColors(containerColor = buttonColor)
                 ) {
-                    Text(text = "Дальше")
+                    Text(text = stringResource(id = R.string.next))
                 }
-
-
-
-
 
                 Spacer(modifier = Modifier.height(1.dp))
             }
