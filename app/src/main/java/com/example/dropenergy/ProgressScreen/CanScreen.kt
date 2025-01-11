de1748741084f531eda315667f7565da8f17b269
Leturgone <finekon221@gmail.com>
2025-01-11 16:11:53 +0300

    Введены ресурсы в CanScreen

--- app/src/main/java/com/example/dropenergy/ProgressScreen/CanScreen.kt
+++ app/src/main/java/com/example/dropenergy/ProgressScreen/CanScreen.kt
@@ -18,12 +18,13 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.dropenergy.database.repository.GetDBState
 import com.example.dropenergy.database.viewModel.DBViewModel
+import com.example.dropenergy.R
 
 @Composable
 fun CanScreen(viewModel:DBViewModel){
@@ -45,7 +46,7 @@ fun CanScreen(viewModel:DBViewModel){
             Column(horizontalAlignment = Alignment.CenterHorizontally) {
 
                 Text(
-                    text = "Не выпито",
+                    text = stringResource(id = R.string.saved_cans),
                     fontSize = 28.sp,
                     color = MaterialTheme.colorScheme.onBackground,
                     fontWeight = FontWeight.Bold,
@@ -58,7 +59,8 @@ fun CanScreen(viewModel:DBViewModel){
                             SavedCans(ekonom_can = ekonom_can)
                         }
                         is GetDBState.Loading ->  CircularProgressIndicator()
-                        is GetDBState.Failure -> Toast.makeText(ctx,"Ошибка сохр банок ", Toast.LENGTH_SHORT).show()
+                        is GetDBState.Failure -> Toast.makeText(ctx, stringResource(id = R.string.loading_saved_cans_err),
+                            Toast.LENGTH_SHORT).show()
                         else -> {null}
                     }
                 }
@@ -67,7 +69,7 @@ fun CanScreen(viewModel:DBViewModel){
 
         }
 
-        Text(text = "Прогноз",
+        Text(text = stringResource(id = R.string.predict),
             fontSize = 24.sp,
             color = MaterialTheme.colorScheme.onBackground,
             fontWeight = FontWeight.Bold,
@@ -90,7 +92,8 @@ fun CanScreen(viewModel:DBViewModel){
                         )
                     }
                     is GetDBState.Loading -> CircularProgressIndicator(Modifier.padding(16.dp))
-                    is GetDBState.Failure -> Toast.makeText(ctx,"Ошибка ежед банок ", Toast.LENGTH_SHORT).show()
+                    is GetDBState.Failure -> Toast.makeText(ctx, stringResource(id = R.string.loading_everyday_cans_err),
+                        Toast.LENGTH_SHORT).show()
                     else -> {null}
                 }
             }
@@ -105,7 +108,7 @@ fun CanScreen(viewModel:DBViewModel){
 @Composable
 fun SavedCans(ekonom_can: Int){
     Text(
-        text = "$ekonom_can банок",
+        text = "$ekonom_can ${stringResource(id = R.string._cans)}",
         fontSize = 24.sp,
         color = MaterialTheme.colorScheme.onBackground,
         fontWeight = FontWeight.Bold,
@@ -116,25 +119,25 @@ fun SavedCans(ekonom_can: Int){
 
 @Composable
 fun FutureCans(in_day_can: Int,in_week_can: Int, in_mounth_can:Int, in_year_can:Int ){
-    Text(text = "$in_day_can банок в день",
+    Text(text = "$in_day_can ${stringResource(id = R.string._cans)} ${stringResource(id = R.string.in_day)}",
         fontSize = 24.sp,
         color = MaterialTheme.colorScheme.onBackground,
         fontWeight = FontWeight.Bold,
         modifier = Modifier.padding(16.dp))
 
-    Text(text = "$in_week_can банок в неделю ",
+    Text(text = "$in_week_can ${stringResource(id = R.string._cans)} ${stringResource(id = R.string.in_week)} ",
         fontSize = 24.sp,
         color = MaterialTheme.colorScheme.onBackground,
         fontWeight = FontWeight.Bold,
         modifier = Modifier.padding(16.dp))
 
-    Text(text = "$in_mounth_can банок в месяц ",
+    Text(text = "$in_mounth_can ${stringResource(id = R.string._cans)} ${stringResource(id = R.string.in_month)} ",
         fontSize = 24.sp,
         color = MaterialTheme.colorScheme.onBackground,
         fontWeight = FontWeight.Bold,
         modifier = Modifier.padding(16.dp))
 
-    Text(text = "$in_year_can банок год ",
+    Text(text = "$in_year_can ${stringResource(id = R.string._cans)} ${stringResource(id = R.string.in_year)} ",
         fontSize = 24.sp,
         color = MaterialTheme.colorScheme.onBackground,
         fontWeight = FontWeight.Bold,
