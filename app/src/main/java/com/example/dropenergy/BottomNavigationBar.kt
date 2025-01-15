2165289e4b721ac547ca8fae2ccb25847c00a572
Leturgone <finekon221@gmail.com>
2025-01-15 19:38:26 +0300

    Сделано разграничение между боттом баром

--- app/src/main/java/com/example/dropenergy/BottomNavigationBar.kt
+++ app/src/main/java/com/example/dropenergy/BottomNavigationBar.kt
@@ -1,12 +1,15 @@
 package com.example.dropenergy
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.AddCircle
 import androidx.compose.material.icons.rounded.BarChart
 import androidx.compose.material.icons.rounded.Build
 import androidx.compose.material.icons.rounded.Create
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
@@ -17,10 +20,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import com.example.dropenergy.data.BottomNavigation
-
+import com.example.dropenergy.ui.theme.LightGreen
 
 
 @Composable
@@ -48,30 +52,38 @@ fun BottomNavigationBar(navController: NavHostController){
             icon = Icons.Rounded.Build
         )
     )
+    Column {
+        Divider(
+            color = LightGreen,
+            thickness = 1.dp,
+            modifier = Modifier.fillMaxWidth()
+        )
+        NavigationBar {
 
+            //Отслеживание текушего маршрута
+            val backStackEntry by navController.currentBackStackEntryAsState()
+            val currentRoute = backStackEntry?.destination?.route
 
-    NavigationBar {
-
-        //Отслеживание текушего маршрута
-        val backStackEntry by navController.currentBackStackEntryAsState()
-        val currentRoute = backStackEntry?.destination?.route
-
-        Row(modifier = Modifier.background((MaterialTheme.colorScheme.inverseOnSurface)))
-        {
-            items.forEach{ item->
-                NavigationBarItem(selected = currentRoute == item.route,
-                    modifier = Modifier.testTag(item.route),
-                    onClick = {
-                        navController.popBackStack()
-                        navController.navigate(item.route)
-                    },
-                    icon = {
-                        Icon(imageVector = item.icon, contentDescription =item.title, tint = MaterialTheme.colorScheme.onBackground)
-                    },
-                    label = {
-                        item.title?.let { Text(text = it, color = MaterialTheme.colorScheme.onBackground) }
-                    })
+            Row(modifier = Modifier.background((MaterialTheme.colorScheme.background)))
+            {
+                items.forEach{ item->
+                    NavigationBarItem(selected = currentRoute == item.route,
+                        modifier = Modifier.testTag(item.route),
+                        onClick = {
+                            navController.popBackStack()
+                            navController.navigate(item.route)
+                        },
+                        icon = {
+                            Icon(imageVector = item.icon,
+                                contentDescription =item.title,
+                                tint = MaterialTheme.colorScheme.primary)
+                        },
+                        label = {
+                            item.title?.let { Text(text = it, color = MaterialTheme.colorScheme.primary) }
+                        })
+                }
             }
         }
     }
+
 }
