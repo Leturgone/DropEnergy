c7faaefc755f03a015b547049dc2991bdc713c44
Leturgone <finekon221@gmail.com>
2025-01-26 23:48:18 +0300

    Сделаны  UI тесты логина

--- app/src/androidTest/java/com/example/dropenergy/RegLogScreenTests/LogScreenUITests.kt
+++ app/src/androidTest/java/com/example/dropenergy/RegLogScreenTests/LogScreenUITests.kt
@@ -8,22 +8,49 @@ import androidx.compose.ui.test.printToLog
 import com.example.dropenergy.LogScreen
 import com.example.dropenergy.MainScreen
 import com.example.dropenergy.RegScreen
+import com.example.dropenergy.StatScreen
+import com.example.dropenergy.database.repository.AuthRepository
+import com.example.dropenergy.database.repository.GetDBState
+import com.example.dropenergy.database.repository.UserRepository
 import com.example.dropenergy.database.viewModel.DBViewModel
+import com.google.firebase.auth.FirebaseUser
+import io.mockk.coEvery
+import io.mockk.every
+import io.mockk.mockk
+import kotlinx.coroutines.flow.MutableStateFlow
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.koin.test.KoinTest
-import org.koin.test.inject
 
-class LogScreenUITests: KoinTest {
-    val viewModel by inject<DBViewModel>()
-
+class LogScreenUITests{
     @get:Rule
     val composeTestRule = createComposeRule()
+    private val authRepository = mockk<AuthRepository>()
+    private val userRepository = mockk<UserRepository>()
+    val mockUser  = mockk<FirebaseUser>(relaxed = true)
+    private lateinit var viewModel: DBViewModel
+    private lateinit var mockStateFlow: MutableStateFlow<GetDBState<FirebaseUser>>
+
+    @Before
+    fun setUP(){
+        coEvery {authRepository.login("giovanni18@pochta.com", "12345678")
+        } returns GetDBState.Success(mockk())
+        coEvery {authRepository.login("1 1 1", "12345678")
+        } returns GetDBState.Failure(mockk())
+        coEvery {authRepository.login("giovanni18@pochta.com", "1")
+        } returns GetDBState.Failure(mockk())
+        coEvery {authRepository.login("giovanni18@pochta.com", "1         ")
+        } returns GetDBState.Failure(mockk())
+        every { authRepository.getCurrentUser() } returns null
+        coEvery {authRepository.login("notlogged@pochta.com", "12345678")
+        } returns GetDBState.Failure(mockk())
+        viewModel = DBViewModel(authRepository, userRepository)
+    }
 
     @Test
     fun testLogScreensGood(){
+
         composeTestRule.setContent { MainScreen(viewModel) }
-        composeTestRule.onRoot().printToLog("MY TAG")
 
         //RegisterScreen
         composeTestRule.onNode(RegScreen.RegTemplate).assertExists()
@@ -37,23 +64,85 @@ class LogScreenUITests: KoinTest {
         composeTestRule.onNode(LogScreen.EmailInput).performTextInput("giovanni18@pochta.com")
         composeTestRule.onNode(LogScreen.PasswordInput).performTextInput("12345678")
         composeTestRule.onNode(LogScreen.NextButton).performClick()
-        composeTestRule.onRoot().printToLog("MY TAG")
 
+        composeTestRule.onNode(StatScreen.ScreenDayCecTemplate).assertExists()
     }
 
     @Test
     fun testLogScreensBadEmail(){
+        composeTestRule.setContent { MainScreen(viewModel) }
 
+        composeTestRule.onNode(RegScreen.RegTemplate).assertExists()
+        composeTestRule.onNode(RegScreen.AlreadyTemplate).assertExists()
+        composeTestRule.onNode(RegScreen.AlreadyTemplate).performClick()
+        //LoginScreen
+        composeTestRule.onNode(LogScreen.LogTemplate).assertExists()
+        composeTestRule.onNode(LogScreen.EmailInput).assertExists()
+        composeTestRule.onNode(LogScreen.PasswordInput).assertExists()
+        composeTestRule.onNode(LogScreen.NextButton).assertExists()
+        composeTestRule.onNode(LogScreen.EmailInput).performTextInput("1 1 1")
+        composeTestRule.onNode(LogScreen.PasswordInput).performTextInput("12345678")
+        composeTestRule.onNode(LogScreen.NextButton).performClick()
+
+        composeTestRule.onNode(StatScreen.ScreenDayCecTemplate).assertDoesNotExist()
+
+    }
+
+    @Test
+    fun testLogScreensShortPassword() {
+        composeTestRule.setContent { MainScreen(viewModel) }
+
+        composeTestRule.onNode(RegScreen.RegTemplate).assertExists()
+        composeTestRule.onNode(RegScreen.AlreadyTemplate).assertExists()
+        composeTestRule.onNode(RegScreen.AlreadyTemplate).performClick()
+        //LoginScreen
+        composeTestRule.onNode(LogScreen.LogTemplate).assertExists()
+        composeTestRule.onNode(LogScreen.EmailInput).assertExists()
+        composeTestRule.onNode(LogScreen.PasswordInput).assertExists()
+        composeTestRule.onNode(LogScreen.NextButton).assertExists()
+        composeTestRule.onNode(LogScreen.EmailInput).performTextInput("giovanni18@pochta.com")
+        composeTestRule.onNode(LogScreen.PasswordInput).performTextInput("1")
+        composeTestRule.onNode(LogScreen.NextButton).performClick()
+
+        composeTestRule.onNode(StatScreen.ScreenDayCecTemplate).assertDoesNotExist()
     }
 
     @Test
     fun testLogScreensBadPassword() {
+        composeTestRule.setContent { MainScreen(viewModel) }
 
+        composeTestRule.onNode(RegScreen.RegTemplate).assertExists()
+        composeTestRule.onNode(RegScreen.AlreadyTemplate).assertExists()
+        composeTestRule.onNode(RegScreen.AlreadyTemplate).performClick()
+        //LoginScreen
+        composeTestRule.onNode(LogScreen.LogTemplate).assertExists()
+        composeTestRule.onNode(LogScreen.EmailInput).assertExists()
+        composeTestRule.onNode(LogScreen.PasswordInput).assertExists()
+        composeTestRule.onNode(LogScreen.NextButton).assertExists()
+        composeTestRule.onNode(LogScreen.EmailInput).performTextInput("giovanni18@pochta.com")
+        composeTestRule.onNode(LogScreen.PasswordInput).performTextInput("1         ")
+        composeTestRule.onNode(LogScreen.NextButton).performClick()
+
+        composeTestRule.onNode(StatScreen.ScreenDayCecTemplate).assertDoesNotExist()
     }
 
     @Test
     fun testLogScreensBadUser(){
+        composeTestRule.setContent { MainScreen(viewModel) }
 
+        composeTestRule.onNode(RegScreen.RegTemplate).assertExists()
+        composeTestRule.onNode(RegScreen.AlreadyTemplate).assertExists()
+        composeTestRule.onNode(RegScreen.AlreadyTemplate).performClick()
+        //LoginScreen
+        composeTestRule.onNode(LogScreen.LogTemplate).assertExists()
+        composeTestRule.onNode(LogScreen.EmailInput).assertExists()
+        composeTestRule.onNode(LogScreen.PasswordInput).assertExists()
+        composeTestRule.onNode(LogScreen.NextButton).assertExists()
+        composeTestRule.onNode(LogScreen.EmailInput).performTextInput("notlogged@pochta.com")
+        composeTestRule.onNode(LogScreen.PasswordInput).performTextInput("12345678")
+        composeTestRule.onNode(LogScreen.NextButton).performClick()
+
+        composeTestRule.onNode(StatScreen.ScreenDayCecTemplate).assertDoesNotExist()
     }
 
 }
