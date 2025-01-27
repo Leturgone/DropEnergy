ca1270a8b671dc523d6cf784150fb9cda1ae3e22
Leturgone <finekon221@gmail.com>
2025-01-27 17:37:20 +0300

    Сделана замена Toast в окне с деньгами

--- app/src/main/java/com/example/dropenergy/ProgressScreen/MoneyScreen.kt
+++ app/src/main/java/com/example/dropenergy/ProgressScreen/MoneyScreen.kt
@@ -1,7 +1,6 @@
 package com.example.dropenergy.ProgressScreen
 
 import android.annotation.SuppressLint
-import android.widget.Toast
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -19,11 +18,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.dropenergy.CustomToastMessage
 import com.example.dropenergy.R
 import com.example.dropenergy.database.repository.GetDBState
 import com.example.dropenergy.database.viewModel.DBViewModel
@@ -40,83 +39,98 @@ fun MoneyScreen(viewModel: DBViewModel){
     var in_year_money by remember { mutableIntStateOf(0) }
     var currency by remember { mutableStateOf<String>("") }
 
-    val ctx = LocalContext.current
+    var showToast by remember { mutableStateOf(false) }
+    var errorMessage by remember { mutableStateOf("") }
+
     LaunchedEffect(Unit) {
         viewModel.getSavedMoney()
         viewModel.getEverydayMoney()
         viewModel.getCurrency()
     }
+    Box(modifier = Modifier.fillMaxWidth()){
+        CustomToastMessage(
+            message = errorMessage,
+            isVisible = showToast,
+            onDismiss = { showToast = false },
+        )
+        Column {
+            Box(Modifier.fillMaxWidth(),contentAlignment = Alignment.Center) {
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Text(
+                        text = stringResource(R.string.saved_money),
+                        fontSize = 28.sp,
+                        color = MaterialTheme.colorScheme.onBackground,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.padding(16.dp)
+                    )
+                    viewModel.currency.collectAsState().value.let {state ->
+                        when(state){
+                            is GetDBState.Success -> {
+                                currency = state.result
+                                viewModel.savedMoneyFlow.collectAsState().value.let {state ->
+                                    when(state){
+                                        is GetDBState.Success -> {
+                                            ekonom_money = state.result
+                                            SavedMoney(currency = currency, ekonom_money =ekonom_money )
+                                        }
+                                        is GetDBState.Loading -> CircularProgressIndicator(Modifier.padding(16.dp))
+                                        is GetDBState.Failure -> {
+                                            errorMessage = stringResource(id = R.string.loading_saved_money_err)
+                                            showToast = true
+                                        }
+                                        else -> {null}
+                                    }
+                                }
 
-    Column {
-        Box(Modifier.fillMaxWidth(),contentAlignment = Alignment.Center) {
-            Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                Text(
-                    text = stringResource(R.string.saved_money),
-                    fontSize = 28.sp,
-                    color = MaterialTheme.colorScheme.onBackground,
-                    fontWeight = FontWeight.Bold,
-                    modifier = Modifier.padding(16.dp)
-                )
-                viewModel.currency.collectAsState().value.let {state ->
+                            }
+                            is GetDBState.Loading -> CircularProgressIndicator(Modifier.padding(16.dp))
+                            is GetDBState.Failure -> {
+                                errorMessage = stringResource(id = R.string.loading_currency_err)
+                                showToast = true
+                            }
+                            else -> {null}
+                        }
+                    }
+
+                }
+
+            }
+
+            Text(text = stringResource(id = R.string.predict),
+                fontSize = 24.sp,
+                color = MaterialTheme.colorScheme.onBackground,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(16.dp))
+
+            Column(Modifier.padding(start = 16.dp)) {
+                viewModel.everydayMoneyFlow.collectAsState().value.let {state ->
                     when(state){
                         is GetDBState.Success -> {
-                            currency = state.result
-                            viewModel.savedMoneyFlow.collectAsState().value.let {state ->
-                                when(state){
-                                    is GetDBState.Success -> {
-                                        ekonom_money = state.result
-                                        SavedMoney(currency = currency, ekonom_money =ekonom_money )
-                                    }
-                                    is GetDBState.Loading -> CircularProgressIndicator(Modifier.padding(16.dp))
-                                    is GetDBState.Failure -> Toast.makeText(ctx,
-                                        stringResource(id = R.string.loading_saved_money_err), Toast.LENGTH_SHORT).show()
-                                    else -> {null}
-                                }
-                            }
-
+                            in_day_money = state.result
+                            in_week_money = in_day_money*7
+                            in_mounth_money = in_day_money*30
+                            in_year_money = in_day_money*365
+                            FutureMoney(
+                                currency = currency,
+                                in_day_money = in_day_money,
+                                in_week_money = in_week_money,
+                                in_mounth_money = in_mounth_money,
+                                in_year_money = in_year_money
+                            )
                         }
                         is GetDBState.Loading -> CircularProgressIndicator(Modifier.padding(16.dp))
-                        is GetDBState.Failure -> Toast.makeText(ctx, stringResource(id = R.string.loading_currency_err),
-                            Toast.LENGTH_SHORT).show()
+                        is GetDBState.Failure ->{
+                            errorMessage = stringResource(id = R.string.loading_everyday_money_err)
+                            showToast = true
+                        }
                         else -> {null}
                     }
                 }
-
             }
 
         }
-
-        Text(text = stringResource(id = R.string.predict),
-            fontSize = 24.sp,
-            color = MaterialTheme.colorScheme.onBackground,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(16.dp))
-
-        Column(Modifier.padding(start = 16.dp)) {
-            viewModel.everydayMoneyFlow.collectAsState().value.let {state ->
-                when(state){
-                    is GetDBState.Success -> {
-                        in_day_money = state.result
-                        in_week_money = in_day_money*7
-                        in_mounth_money = in_day_money*30
-                        in_year_money = in_day_money*365
-                        FutureMoney(
-                            currency = currency,
-                            in_day_money = in_day_money,
-                            in_week_money = in_week_money,
-                            in_mounth_money = in_mounth_money,
-                            in_year_money = in_year_money
-                        )
-                    }
-                    is GetDBState.Loading -> CircularProgressIndicator(Modifier.padding(16.dp))
-                    is GetDBState.Failure -> Toast.makeText(ctx, stringResource(id = R.string.loading_everyday_money_err),
-                        Toast.LENGTH_SHORT).show()
-                    else -> {null}
-                }
-            }
-        }
-
     }
+
 }
 
 @Composable
