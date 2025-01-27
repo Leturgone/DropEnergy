2f59db7efe090c350e3cb49a655d7b57bf4955dd
Leturgone <finekon221@gmail.com>
2025-01-27 16:19:19 +0300

    Сделана замена Toast в диалоговом окне рега

--- app/src/main/java/com/example/dropenergy/EnterDialogScreen/RegisterScreen.kt
+++ app/src/main/java/com/example/dropenergy/EnterDialogScreen/RegisterScreen.kt
@@ -42,6 +42,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.example.dropenergy.CustomToastMessage
 import com.example.dropenergy.R
 import com.example.dropenergy.database.repository.GetDBState
 import com.example.dropenergy.database.viewModel.DBViewModel
@@ -58,6 +59,9 @@ fun RegScreen(navController: NavHostController, viewModel: DBViewModel?){
     var passwordVisible by remember { mutableStateOf(false) }
     var loginOK by remember { mutableStateOf(false) }
     var buttonColor by remember { mutableStateOf(LightYellow) }
+
+    var showToast by remember { mutableStateOf(false) }
+    var errorMessage by remember { mutableStateOf("") }
 
     val loginState = viewModel?.loginFlow?.collectAsState()
     loginState?.value.let {state ->
@@ -77,7 +81,6 @@ fun RegScreen(navController: NavHostController, viewModel: DBViewModel?){
     }
 
 
-    val ctx = LocalContext.current
     val emailErr = stringResource(id = R.string.email_err)
     val passwordErr = stringResource(id = R.string.password_err)
     val shortPasswordErr = stringResource(id = R.string.password_short_err)
@@ -90,88 +93,98 @@ fun RegScreen(navController: NavHostController, viewModel: DBViewModel?){
                 .padding(16.dp),
             color = MaterialTheme.colorScheme.primary
         )
-        Box(Modifier.fillMaxWidth(),contentAlignment = Alignment.Center) {
-            Column( modifier = Modifier.fillMaxSize(),
-                horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.SpaceBetween) {
-                Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                    Spacer(modifier = Modifier.height(60.dp))
-                    Text(
-                        text = stringResource(id = R.string.registration),
-                        fontSize = 28.sp,
-                        color = MaterialTheme.colorScheme.onBackground,
-                        fontWeight = FontWeight.Bold,
-                        modifier = Modifier.padding(16.dp),
-                        textAlign = TextAlign.Center
-                    )
-                    Spacer(modifier = Modifier.height(60.dp))
+        Box(modifier = Modifier.fillMaxWidth()){
+            CustomToastMessage(
+                message = errorMessage,
+                isVisible = showToast,
+                onDismiss = { showToast = false },
+            )
+            Box(Modifier.fillMaxWidth(),contentAlignment = Alignment.Center) {
+                Column( modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.SpaceBetween) {
+                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                        Spacer(modifier = Modifier.height(60.dp))
+                        Text(
+                            text = stringResource(id = R.string.registration),
+                            fontSize = 28.sp,
+                            color = MaterialTheme.colorScheme.onBackground,
+                            fontWeight = FontWeight.Bold,
+                            modifier = Modifier.padding(16.dp),
+                            textAlign = TextAlign.Center
+                        )
+                        Spacer(modifier = Modifier.height(60.dp))
 
-                    OutlinedTextField(
-                        value = loginInputText,
-                        label = { Text(stringResource(id = R.string.email)) },
-                        singleLine = true,
-                        keyboardOptions =  KeyboardOptions(keyboardType = KeyboardType.Email),
-                        onValueChange = {
-                            loginInputText = it
-                            loginOK = true
+                        OutlinedTextField(
+                            value = loginInputText,
+                            label = { Text(stringResource(id = R.string.email)) },
+                            singleLine = true,
+                            keyboardOptions =  KeyboardOptions(keyboardType = KeyboardType.Email),
+                            onValueChange = {
+                                loginInputText = it
+                                loginOK = true
+                            })
+                        Spacer(modifier = Modifier.height(60.dp))
+
+                        OutlinedTextField(
+                            value = passwordInputText,
+                            singleLine = true,
+                            label = { Text(stringResource(id = R.string.password)) },
+                            visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+                            keyboardOptions =  KeyboardOptions(keyboardType = KeyboardType.Password),
+                            onValueChange = {
+                                passwordInputText = it
+                                if (loginOK){
+                                    buttonColor = LightGreen
+                                }
+                            },
+                            trailingIcon = {
+                                val image = if (passwordVisible)
+                                    Icons.Filled.Visibility
+                                else Icons.Filled.VisibilityOff
+
+                                val description = if (passwordVisible) "Hide password" else "Show password"
+
+                                IconButton(onClick = {passwordVisible = !passwordVisible}){
+                                    Icon(imageVector  = image, description)
+                                }
+                            })
+                    }
+
+                    Button(onClick = {
+                        if( loginInputText.isEmpty() || !loginInputText.matches("^[A-Za-z0-9@.]+$".toRegex())
+                            || loginInputText.matches("\\s".toRegex())){
+                            errorMessage = emailErr
+                            showToast = true
+                        }
+                        else if( passwordInputText.isEmpty() || !passwordInputText.matches("^[A-Za-z0-9]+$".toRegex())
+                            || passwordInputText.matches("\\s".toRegex())){
+                            errorMessage = passwordErr
+                            showToast = true
+                        }
+                        else if (passwordInputText.length < 8){
+                            errorMessage = shortPasswordErr
+                            showToast = true
+                        }
+                        else {
+                            //Загрузка в бд
+                            viewModel?.createUser(loginInputText,passwordInputText)
+                            navController.navigate("dialog_cans")
+
+                        }
+
+                    },
+                        colors = ButtonDefaults.buttonColors(containerColor = buttonColor)
+                    ) {
+                        Text(text = stringResource(id = R.string.next))
+                    }
+                    Text(text = stringResource(id = R.string.already_have),
+                        color = MaterialTheme.colorScheme.primary,
+                        modifier = Modifier.clickable {
+                            navController.navigate("login")
                         })
-                    Spacer(modifier = Modifier.height(60.dp))
 
-                    OutlinedTextField(
-                        value = passwordInputText,
-                        singleLine = true,
-                        label = { Text(stringResource(id = R.string.password)) },
-                        visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
-                        keyboardOptions =  KeyboardOptions(keyboardType = KeyboardType.Password),
-                        onValueChange = {
-                            passwordInputText = it
-                            if (loginOK){
-                                buttonColor = LightGreen
-                            }
-                        },
-                        trailingIcon = {
-                            val image = if (passwordVisible)
-                                Icons.Filled.Visibility
-                            else Icons.Filled.VisibilityOff
-
-                            val description = if (passwordVisible) "Hide password" else "Show password"
-
-                            IconButton(onClick = {passwordVisible = !passwordVisible}){
-                                Icon(imageVector  = image, description)
-                            }
-                        })
+                    Spacer(modifier = Modifier.height(1.dp))
                 }
-
-                Button(onClick = {
-                    if( loginInputText.isEmpty() || !loginInputText.matches("^[A-Za-z0-9@.]+$".toRegex())
-                        || loginInputText.matches("\\s".toRegex())){
-                        Toast.makeText(ctx, emailErr, Toast.LENGTH_SHORT).show()
-                    }
-                    else if( passwordInputText.isEmpty() || !passwordInputText.matches("^[A-Za-z0-9]+$".toRegex())
-                        || passwordInputText.matches("\\s".toRegex())){
-                        Toast.makeText(ctx, passwordErr, Toast.LENGTH_SHORT).show()
-                    }
-                    else if (passwordInputText.length < 8){
-                        Toast.makeText(ctx, shortPasswordErr, Toast.LENGTH_SHORT).show()
-                    }
-                    else {
-                        //Загрузка в бд
-                        viewModel?.createUser(loginInputText,passwordInputText)
-                        navController.navigate("dialog_cans")
-
-                    }
-
-                },
-                    colors = ButtonDefaults.buttonColors(containerColor = buttonColor)
-                ) {
-                    Text(text = stringResource(id = R.string.next))
-                }
-                Text(text = stringResource(id = R.string.already_have),
-                    color = MaterialTheme.colorScheme.primary,
-                    modifier = Modifier.clickable {
-                        navController.navigate("login")
-                    })
-
-                Spacer(modifier = Modifier.height(1.dp))
             }
         }
     }
