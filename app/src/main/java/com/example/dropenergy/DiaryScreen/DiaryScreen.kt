ec1d8c999fe28dfe3504560ec65d67b35872c7ec
Leturgone <finekon221@gmail.com>
2025-01-27 17:35:00 +0300

    Сделана замена Toast в окне с дневником

--- app/src/main/java/com/example/dropenergy/DiaryScreen/DiaryScreen.kt
+++ app/src/main/java/com/example/dropenergy/DiaryScreen/DiaryScreen.kt
@@ -1,10 +1,10 @@
 package com.example.dropenergy.DiaryScreen
 
-import android.widget.Toast
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -24,51 +24,63 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.dropenergy.CustomToastMessage
+import com.example.dropenergy.R
 import com.example.dropenergy.data.DiaryRecord
 import com.example.dropenergy.database.repository.GetDBState
 import com.example.dropenergy.database.viewModel.DBViewModel
-import com.example.dropenergy.R
-
 
 
 @Composable
 fun DiaryScreen(viewModel:DBViewModel){
     var diary by remember { mutableStateOf(listOf<Pair<String,DiaryRecord>>()) }
-    val ctx = LocalContext.current
+
+    var showToast by remember { mutableStateOf(false) }
+    var errorMessage by remember { mutableStateOf("") }
+
     LaunchedEffect(Unit) {
         viewModel.getDiary()
     }
-    Column {
-        Text(text = stringResource(id = R.string.diary),
-            fontSize = 24.sp,
-            color = MaterialTheme.colorScheme.onBackground,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(16.dp)
+    Box(modifier = Modifier.fillMaxWidth()){
+        CustomToastMessage(
+            message = errorMessage,
+            isVisible = showToast,
+            onDismiss = { showToast = false },
         )
-        Box(
-            modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center
-        ) {
+        Column {
+            Text(text = stringResource(id = R.string.diary),
+                fontSize = 24.sp,
+                color = MaterialTheme.colorScheme.onBackground,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(16.dp)
+            )
+            Box(
+                modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center
+            ) {
 
-            viewModel.diaryFlow.collectAsState().value.let {state ->
-                when(state){
-                    is GetDBState.Success -> {
-                        diary = state.result.toList()
-                        DiaryList(diary = diary)
+                viewModel.diaryFlow.collectAsState().value.let {state ->
+                    when(state){
+                        is GetDBState.Success -> {
+                            diary = state.result.toList()
+                            DiaryList(diary = diary)
+                        }
+                        is GetDBState.Loading -> CircularProgressIndicator()
+                        is GetDBState.Failure -> {
+                            errorMessage = stringResource(id = R.string.loading_diary_err)
+                            showToast = true
+                        }
+                        else -> {null}
                     }
-                    is GetDBState.Loading -> CircularProgressIndicator()
-                    is GetDBState.Failure -> Toast.makeText(ctx, stringResource(id = R.string.loading_diary_err),
-                        Toast.LENGTH_SHORT).show()
-                    else -> {null}
                 }
-            }
 
+            }
         }
     }
+
 
 }
 
